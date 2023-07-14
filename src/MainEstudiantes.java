07e2cf2bfc22f3413c966100f767f8cde7336478
Cristian <cristian.paredes01@epn.edu.ec>
2023-07-14 12:35:06 -0500

    Initial commit

---
+++ src/MainEstudiantes.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainEstudiantes {
+    private DefaultListModel<String> estudiantesListModel;
+
+    public MainEstudiantes() {
+        JFrame frame = new JFrame("Registro de Estudiantes");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+
+        estudiantesListModel = new DefaultListModel<>();
+        JList<String> estudiantesList = new JList<>(estudiantesListModel);
+        JScrollPane scrollPane = new JScrollPane(estudiantesList);
+
+        frame.add(scrollPane, BorderLayout.CENTER);
+
+        PanelEstudiantes panelEstudiantes = new PanelEstudiantes(estudiantesListModel);
+        frame.add(panelEstudiantes, BorderLayout.WEST);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        }
+    }
